2a0e0efe7eef5101da0cb0d88806d75140debb75
Hreamer <reamer.hudson@gmail.com>
2023-03-19 17:07:28 -0400

    Capitiled K

--- system/kernel.c
+++ system/kernel.c
@@ -8,5 +8,5 @@ void kernel_main(void)
 	/* Initialize terminal interface */
 	terminal_initialize();
  
-	terminal_writestring("Hello, kernel World!\n");
+	terminal_writestring("Hello, Kernel World \n");
 }
